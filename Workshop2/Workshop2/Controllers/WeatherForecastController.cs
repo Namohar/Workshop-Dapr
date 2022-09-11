a4f7dbd4bf9e4bdd609f2454106cc44a548cacde
sshrav <sshrav@users.noreply.github.com>
2022-09-11 22:14:55 +0530

    Workshop2

---
+++ Workshop2/Workshop2/Controllers/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Dapr.Client;
+namespace Workshop2.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "ProxyWeatherForecast")]
+    public async Task<IEnumerable<WeatherForecast>> Get()
+    {
+        HttpClient client = DaprClient.CreateInvokeHttpClient("weather");
+        var weatherForecasts =
+            await client.GetFromJsonAsync<List<WeatherForecast>>("weatherforecast");
+
+        return weatherForecasts;
+    }
+}
+
